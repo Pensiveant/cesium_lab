485d3e69b3b2613c628cdd7abc280a6870e026df
pensiveant <142905570003@qq.com>
2023-05-25 10:44:50 +0800

    feat: SceneView添加修改鼠标默认行为方法_modifyMouseAction

--- src/utils/SceneView.js
+++ src/utils/SceneView.js
@@ -20,7 +20,39 @@ class SceneView {
     });
 
     viewer.cesiumWidget.creditContainer.style.display = "none"; // 去除logo
+    this._modifyMouseAction(viewer);
     return viewer;
+  }
+
+  /**
+   * 修改鼠标操作的默认行为（左键：移动；右键：放大缩小；中键：旋转）
+   * @param {*} viewer
+   */
+  _modifyMouseAction(viewer) {
+    //修改旋转为右键操作
+    viewer.scene.screenSpaceCameraController.tiltEventTypes = [
+      Cesium.CameraEventType.RIGHT_DRAG,
+      Cesium.CameraEventType.PINCH,
+      {
+        eventType: Cesium.CameraEventType.LEFT_DRAG,
+        modifier: Cesium.KeyboardEventModifier.CTRL,
+      },
+      {
+        eventType: Cesium.CameraEventType.RIGHT_DRAG,
+        modifier: Cesium.KeyboardEventModifier.CTRL,
+      },
+    ];
+    // 重置地图放大缩小操作，
+    viewer.scene.screenSpaceCameraController.zoomEventTypes = [
+      Cesium.CameraEventType.MIDDLE_DRAG,
+      Cesium.CameraEventType.WHEEL,
+      Cesium.CameraEventType.PINCH,
+    ];
+    
+    //移除默认鼠标左键双击事件
+    viewer.screenSpaceEventHandler.removeInputAction(
+      Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK
+    );
   }
 }
 
