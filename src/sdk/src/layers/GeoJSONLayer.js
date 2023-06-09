6900466925fe040b0a089ba3e1777b07fbc0be86
pensiveant <142905570003@qq.com>
2023-06-09 15:36:59 +0800

    feat: sdk geojson定位初稿

--- src/sdk/src/layers/GeoJSONLayer.js
+++ src/sdk/src/layers/GeoJSONLayer.js
@@ -1,4 +1,5 @@
 import Layer from "./Layer.js";
+import { bbox } from "../Helper.js";
 
 class GeoJSONLayer extends Layer {
   constructor(options) {
@@ -7,9 +8,24 @@ class GeoJSONLayer extends Layer {
     this.type = "geojson";
     this.source = options?.source;
     this.url = options?.url;
+    this._init();
+  }
+
+  _init() {
+    // 初始化范围
+    let extent = bbox(this.source);
+    this.extent = {
+      xmin: extent[0],
+      xmax: extent[2],
+      ymin: extent[1],
+      ymax: extent[3],
+    };
   }
 
   async _loadData() {
+    if (this._data) {
+      return this._data;
+    }
     let geojsonDataSouce;
     if (this.source) {
       geojsonDataSouce = await Cesium.GeoJsonDataSource.load(this.source);
@@ -20,7 +36,12 @@ class GeoJSONLayer extends Layer {
         url: this.url,
       });
     }
+    this._data = geojsonDataSouce;
     return geojsonDataSouce;
+  }
+
+  zoomTo(viewer) {
+    viewer.flyTo(this._data);
   }
 }
 
