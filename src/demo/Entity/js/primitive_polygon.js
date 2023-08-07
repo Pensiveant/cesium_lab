d58537ca47a798488cdd669b3295129f4a2e7362
pensiveant <142905570003@qq.com>
2023-08-07 15:30:25 +0800

    feat: 添加示例primitive_polygon

---
+++ src/demo/Entity/js/primitive_polygon.js
@@ -0,0 +1,70 @@
+// const Cesium = require("cesium"); // 智能提示使用
+
+Cesium.Ion.defaultAccessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhYzVkMTc1ZS00NTRhLTRjY2QtYTQwZS01YmU2Mjg1ODAwN2YiLCJpZCI6MjU5LCJpYXQiOjE2ODgzOTgwMjl9.MZC_HUBRd0y5HJeB-F5QSpT-fEgTM6mI5gMaSND9FHc";
+
+// 初始化view
+const viewer = new Cesium.Viewer("cesiumContainer", {
+  geocoder: false, // 搜索功能
+  homeButton: false, // home
+  sceneModePicker: false, // 隐藏二三维切换
+  baseLayerPicker: false, //隐藏默认底图选择控件
+  navigationHelpButton: false, // 帮助？号
+  animation: false,
+  infoBox: false,
+  timeline: false,
+  fullscreenButton: false,
+  // 使用ArcGIS 底图
+  imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
+    url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
+  }),
+  terrain: Cesium.Terrain.fromWorldTerrain(),
+});
+viewer.cesiumWidget.creditContainer.style.display = "none"; // 去除logo
+viewer.scene.globe.depthTestAgainstTerrain = true;
+
+const centerPoint = [104.06335998805471, 30.659858531850176, 500]; // 中心点的经度、纬度、高程
+const point = new Cesium.Entity({
+  position: Cesium.Cartesian3.fromDegrees(
+    centerPoint[0],
+    centerPoint[1],
+    centerPoint[2]
+  ),
+  point: {
+    pixelSize: 5,
+    color: Cesium.Color.RED,
+    outlineColor: Cesium.Color.WHITE,
+    outlineWidth: 2,
+  },
+});
+viewer.entities.add(point);
+
+let positions = Cesium.Cartesian3.fromDegreesArrayHeights([
+  104.06204432699153, 30.660726592602778, 458.0359123306434, 104.06491131775273,
+  30.660792356429575, 457.5663362739622, 104.06488421424214, 30.65902970314711,
+  462.1401663208093, 104.06206037235002, 30.65901615923885, 458.80041016490196,
+]);
+let polygonGeometry = new Cesium.PolygonGeometry({
+  polygonHierarchy: new Cesium.PolygonHierarchy(positions),
+  perPositionHeight: true,
+});
+
+let polygonPrimitive = new Cesium.Primitive({
+  geometryInstances: new Cesium.GeometryInstance({
+    geometry: polygonGeometry,
+  }),
+  appearance: new Cesium.EllipsoidSurfaceAppearance({
+    material: new Cesium.Material.fromType("Color", {
+      color: new Cesium.Color(1.0, 0.0, 0.0, 0.3),
+    }),
+  }),
+});
+viewer.scene.primitives.add(polygonPrimitive);
+
+viewer.camera.flyTo({
+  destination: Cesium.Cartesian3.fromDegrees(
+    104.06204432699153,
+    30.660726592602778,
+    1500
+  ),
+});
