024ecc87ad82c43678388bf902f4a1216e996555
pensiveant <142905570003@qq.com>
2023-08-07 14:38:16 +0800

    feat: primitive box 加载示例

---
+++ src/demo/Entity/js/primitive_box.js
@@ -0,0 +1,75 @@
+// const Cesium = require("cesium"); // 智能提示使用
+
+Cesium.Ion.defaultAccessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhYzVkMTc1ZS00NTRhLTRjY2QtYTQwZS01YmU2Mjg1ODAwN2YiLCJpZCI6MjU5LCJpYXQiOjE2ODgzOTgwMjl9.MZC_HUBRd0y5HJeB-F5QSpT-fEgTM6mI5gMaSND9FHc";
+
+// 初始化view
+const viewer = new Cesium.Viewer("cesiumContainer", {
+  geocoder: false, // 搜索功能
+  homeButton: false, // home
+  sceneModePicker: false, // 隐藏二三维切换
+  baseLayerPicker: false, //隐藏默认底图选择控件
+  navigationHelpButton: false, // 帮助？号
+  animation: false,
+  infoBox: false,
+  timeline: false,
+  fullscreenButton: false,
+  // 使用ArcGIS 底图
+  imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
+    url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
+  }),
+  terrain: Cesium.Terrain.fromWorldTerrain(),
+});
+viewer.cesiumWidget.creditContainer.style.display = "none"; // 去除logo
+viewer.scene.globe.depthTestAgainstTerrain = true;
+
+const centerPoint = [104.06335998805471, 30.659858531850176, 500]; // 中心点的经度、纬度、高程
+const point = new Cesium.Entity({
+  position: Cesium.Cartesian3.fromDegrees(
+    centerPoint[0],
+    centerPoint[1],
+    centerPoint[2]
+  ),
+  point: {
+    pixelSize: 5,
+    color: Cesium.Color.RED,
+    outlineColor: Cesium.Color.WHITE,
+    outlineWidth: 2,
+  },
+});
+viewer.entities.add(point);
+
+let box = new Cesium.BoxGeometry({
+  vertexFormat: Cesium.VertexFormat.POSITION_ONLY,
+  // 等价于使用六个平面，截取出长方体
+  maximum: new Cesium.Cartesian3(20.0, 40.0, 60.0),     // x,y,z坐标的最大值
+  minimum: new Cesium.Cartesian3(-20.0, -40.0, -60.0),  // x,y,z坐标的最小值
+});
+let boxGeometry = Cesium.BoxGeometry.createGeometry(box);
+  
+const center = Cesium.Cartesian3.fromDegrees(
+  104.06335998805471,
+  30.659858531850176,
+  500
+);
+const transform = Cesium.Transforms.eastNorthUpToFixedFrame(center);
+let boxPrimitive = new Cesium.Primitive({
+  modelMatrix: transform,   // 模型变换矩阵，将长方体，变换到世界坐标中
+  geometryInstances: new Cesium.GeometryInstance({
+    geometry: box,
+  }),
+  appearance: new Cesium.EllipsoidSurfaceAppearance({
+    material: new Cesium.Material.fromType("Color", {
+      color: new Cesium.Color(1.0, 0.0, 0.0, 0.3),
+    }),
+  }),
+});
+viewer.scene.primitives.add(boxPrimitive);
+
+viewer.camera.flyTo({
+  destination: Cesium.Cartesian3.fromDegrees(
+    104.06335998805471,
+    30.659858531850176,
+    1500
+  ),
+});
