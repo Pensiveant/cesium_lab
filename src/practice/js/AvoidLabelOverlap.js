9ad259774fe4dc7f3b85c66f3fe7c0cd5655f007
pensiveant <142905570003@qq.com>
2023-08-17 16:31:41 +0800

    feat: 注记避让算法优化，同时兼容Label实体避让

--- src/practice/js/AvoidLabelOverlap.js
+++ src/practice/js/AvoidLabelOverlap.js
@@ -35,30 +35,52 @@ let pointGeojson = Cesium.GeoJsonDataSource.load("./data/tianhe.geojson", {
   var entities = dataSource.entities.values;
   for (var i = 0; i < entities.length; i++) {
     var entity = entities[i];
-
     entity.point = new Cesium.PointGraphics({
       color: Cesium.Color.RED,
       pixelSize: 1,
     });
+
+    // // entity为label
+    // entity.billboard.show = false;
+    // entity.label = {
+    //   text: entity._name,
+    //   font: "20px sans-serif",
+    // };
+
+    // entity 为billboard
+    entity.billboard.show = true;
     const canvas = writeTextToCanvas(entity._name, {
       font: "20px sans-serif",
     });
     entity.billboard.image = canvas;
     entity.billboard.horizontalOrigin = Cesium.HorizontalOrigin.LEFT;
-    entity.billboard.verticalOrigin = Cesium.VerticalOrigin.CENTER;
+    entity.billboard.verticalOrigin = Cesium.VerticalOrigin.TOP;
     entity.billboard.width = canvas.width;
     entity.billboard.height = canvas.height;
   }
   viewer.flyTo(dataSource);
-  let test = new LabelAvoid({ viewer, dataSource });
+  let labelAvoid = new LabelAvoid({
+    viewer,
+    dataSource,
+    offsetX: 10,
+    offsetY: 10,
+  });
 });
 
 //#region 注记避让
 
 class LabelAvoid {
   constructor(options) {
+    options = Cesium.defaultValue(options, Cesium.defaultValue.EMPTY_OBJECT);
     this.dataSource = options.dataSource;
     this.viewer = options.viewer;
+    this.offsetX = Cesium.defaultValue(options.offsetX, 0); // X轴，左右偏移量
+    this.offsetY = Cesium.defaultValue(options.offsetY, 0); // Y轴，上下偏移量
+    this._removeEventListener = undefined;
+    this._initialize();
+  }
+
+  _initialize() {
     this.setAvoidLalelOverlap();
   }
 
@@ -68,11 +90,29 @@ class LabelAvoid {
     this._removeEventListener = this.viewer.camera.changed.addEventListener(
       () => {
         const entities = dataSource.entities.values;
-        that.avoidLalelOverlap(entities, 10, 10); // 上下左右空半个字大小
+        let entityType =
+          entities[0].label && entities[0].label.text._value.length > 0
+            ? "label"
+            : "billboard";
+        that.avoidLalelOverlap(
+          entities,
+          this.offsetX,
+          this.offsetY,
+          entityType
+        ); // 上下左右空半个字大小
       }
     );
   }
-  avoidLalelOverlap(entities, offsetX = 0, offsetY = 0) {
+
+  /**
+   *
+   * @param {*} entities
+   * @param {*} offsetX
+   * @param {*} offsetY
+   * @param {*} entityType
+   * @returns
+   */
+  avoidLalelOverlap(entities, offsetX, offsetY, entityType = "billboard") {
     if (!entities || entities.length === 0) {
       return;
     }
@@ -81,14 +121,63 @@ class LabelAvoid {
     for (let i = 0; i < entities.length; i++) {
       const item = entities[i];
       const coord = Cesium.SceneTransforms.wgs84ToDrawingBufferCoordinates(
-        viewer.scene,
+        this.viewer.scene,
         item.position._value
       );
+
+      //#region 将所有平面坐标，移动到左中点。
+      const billboard = item.billboard;
+      let width = 0;
+      let height = 0;
+      if (entityType === "billboard") {
+        width = billboard.width;
+        height = billboard.height;
+        const scale = billboard.scale ?? 1;
+        width *= scale;
+        height *= scale;
+      }
+      if (entityType === "label") {
+        const canvas = writeTextToCanvas(item.label.text._value, {
+          font: item.label.font._value,
+        });
+        width = canvas.width;
+        height = canvas.height;
+        item.label.width = width;
+        item.label.height = height;
+      }
+
+      let x = coord.x;
+      const horizontalOrigin = billboard.horizontalOrigin
+        ? billboard.horizontalOrigin._value
+        : Cesium.HorizontalOrigin.CENTER;
+      if (horizontalOrigin === Cesium.HorizontalOrigin.RIGHT) {
+        x = x - width; // x坐标全部转换到左边进行判断
+      } else if (horizontalOrigin === Cesium.HorizontalOrigin.CENTER) {
+        x = x - width / 2;
+      }
+
+      let y = coord.y;
+      const verticalOrigin = billboard.verticalOrigin._value
+        ? billboard.verticalOrigin._value
+        : Cesium.VerticalOrigin.CENTER;
+      if (
+        verticalOrigin === Cesium.VerticalOrigin.BOTTOM ||
+        verticalOrigin === Cesium.VerticalOrigin.BASELINE
+      ) {
+        y = y - height / 2; //  y坐标全部转换到中间位置
+      } else if (verticalOrigin === Cesium.VerticalOrigin.TOP) {
+        y = y + height / 2;
+      }
+      //#endregion
+
       if (coord) {
         points.push({
           index: i,
           entity: item,
-          coord, // 屏幕坐标
+          coord: {
+            x,
+            y,
+          }, // 屏幕坐标
           hide: false, // 是否隐藏
         });
       }
@@ -100,9 +189,14 @@ class LabelAvoid {
     }
     index.finish();
 
+    // 避让前，全部显示
     for (let i = 0; i < entities.length; i++) {
       const item = entities[i];
-      item.billboard.show = true;
+      if (entityType === "billboard") {
+        item.billboard.show = true;
+      } else if (entityType === "label") {
+        item.label.show = true;
+      }
     }
 
     // 遮挡检测
@@ -111,47 +205,24 @@ class LabelAvoid {
       if (target.hide) {
         continue;
       }
-      //   const bbox = target.coord;
-      const bbox = this.getScreenSpaceBoundingBox(
-        target.entity.billboard,
-        target.coord
-      );
 
-      const range = {
-        minx: 0,
-        miny: 0,
-        maxx: 0,
-        maxy: 0,
-      };
-      const verticalOrigin = target.entity.billboard.verticalOrigin._value
-        ? target.entity.billboard.verticalOrigin._value
-        : Cesium.VerticalOrigin.CENTER;
-      if (verticalOrigin === Cesium.VerticalOrigin.TOP) {
-        range.miny = bbox.y - offsetY;
-        range.maxy = bbox.y + bbox.height + offsetY;
-      } else if (verticalOrigin === Cesium.VerticalOrigin.CENTER) {
-        range.miny = bbox.y - bbox.height / 2 - offsetY;
-        range.maxy = bbox.y + bbox.height / 2 + offsetY;
-      } else if (
-        verticalOrigin === Cesium.VerticalOrigin.BASELINE ||
-        verticalOrigin === Cesium.VerticalOrigin.BOTTOM
-      ) {
-        range.miny = bbox.y - bbox.height - offsetY;
-        range.maxy = bbox.y + bbox.height + offsetY;
+      // 获取范围框
+      let range;
+      if (entityType === "billboard") {
+        range = this.getScreenSpaceBoundingBox(
+          target.entity.billboard,
+          target.coord,
+          offsetX,
+          offsetY
+        );
       }
-
-      const horizontalOrigin = target.entity.billboard.horizontalOrigin
-        ? target.entity.billboard.horizontalOrigin._value
-        : Cesium.HorizontalOrigin.CENTER;
-      if (horizontalOrigin === Cesium.HorizontalOrigin.CENTER) {
-        range.minx = bbox.x - bbox.width / 2 - offsetX;
-        range.maxx = bbox.x + bbox.width / 2 + offsetX;
-      } else if (horizontalOrigin === Cesium.HorizontalOrigin.LEFT) {
-        range.minx = bbox.x - offsetX;
-        range.maxx = bbox.x + bbox.width + offsetX;
-      } else if (horizontalOrigin === Cesium.HorizontalOrigin.RIGHT) {
-        range.minx = bbox.x - bbox.width - offsetX;
-        range.maxx = bbox.x + offsetX;
+      if (entityType === "label") {
+        range = this.getScreenSpaceBoundingBox(
+          target.entity.label,
+          target.coord,
+          offsetX,
+          offsetY
+        );
       }
 
       const neighbors = index.range(
@@ -161,54 +232,76 @@ class LabelAvoid {
         range.maxy
       );
 
-      let startIndex = offsetX === 0 ? 0 : 1; // offsetX不为0时，自己也会被包含在内，如果隐藏，将全部看不见
-      for (let j = startIndex; j < neighbors.length; j++) {
+      for (let j = 0; j < neighbors.length; j++) {
         const neighborsPoint = points[neighbors[j]];
-        neighborsPoint.entity.billboard.show = false;
+        if (
+          target.coord.x === neighborsPoint.coord.x &&
+          target.coord.y === neighborsPoint.coord.y
+        ) {
+          continue;
+        }
+
+        if (entityType === "billboard") {
+          neighborsPoint.entity.billboard.show = false;
+        } else if (entityType === "label") {
+          neighborsPoint.entity.label.show = false;
+        }
         neighborsPoint.hide = true;
       }
     }
   }
 
-  getScreenSpaceBoundingBox(billboard, screenSpacePosition, result) {
+  /**
+   *
+   * @param {*} billboard||Label
+   * @param {*} screenSpacePosition
+   * @param {*} offsetX
+   * @param {*} offsetY
+   * @returns
+   */
+  getScreenSpaceBoundingBox(
+    billboard,
+    screenSpacePosition,
+    offsetX = 0,
+    offsetY = 0
+  ) {
+    let minx = 0;
+    let maxx = 0;
+    let miny = 0;
+    let maxy = 0;
+
     let width = billboard.width;
     let height = billboard.height;
-
     const scale = billboard.scale ?? 1;
     width *= scale;
     height *= scale;
 
     let x = screenSpacePosition.x;
-    let horizontalOrigin =
-      billboard.horizontalOrigin ?? Cesium.HorizontalOrigin.CENTER;
-    if (horizontalOrigin === Cesium.HorizontalOrigin.RIGHT) {
-      x -= width;
-    } else if (horizontalOrigin === Cesium.HorizontalOrigin.CENTER) {
-      x -= width * 0.5;
-    }
+    minx = x - offsetX;
+    maxx = x + width + offsetX;
 
     let y = screenSpacePosition.y;
-    let verticalOrigin =
-      billboard.verticalOrigin ?? Cesium.VerticalOrigin.CENTER;
-    if (
-      verticalOrigin === Cesium.VerticalOrigin.BOTTOM ||
-      verticalOrigin === Cesium.VerticalOrigin.BASELINE
-    ) {
-      y -= height;
-    } else if (verticalOrigin === Cesium.VerticalOrigin.CENTER) {
-      y -= height * 0.5;
+    miny = y - height - offsetY;
+    maxy = y + height + offsetY;
+    return { minx, maxx, miny, maxy };
+  }
+
+  destroy() {
+    const entities = this.dataSource.entities.values;
+    for (let i = 0; i < entities.length; i++) {
+      const item = entities[i];
+      if (item.billboard) {
+        item.billboard.show = true;
+      }
+
+      if (item.label) {
+        item.label.show = true;
+      }
     }
 
-    if (!Cesium.defined(result)) {
-      result = new Cesium.BoundingRectangle();
+    if (this._removeEventListener) {
+      this._removeEventListener();
     }
-
-    result.x = x;
-    result.y = y;
-    result.width = width;
-    result.height = height;
-
-    return result;
   }
 }
 window.LabelAvoid = LabelAvoid;
