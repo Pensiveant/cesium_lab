9a665b21bebc94c46193fde6a0a135c16e20b14b
pensiveant <142905570003@qq.com>
2024-06-06 17:11:02 +0800

    feat:ElevationRamp示例

---
+++ src/demo/Material/js/materialElevationRamp.js
@@ -0,0 +1,39 @@
+// const Cesium = require("cesium"); // 智能提示使用
+
+Cesium.Ion.defaultAccessToken = defaultAccessToken;
+// 初始化view
+const viewer = new Cesium.Viewer("cesiumContainer", {
+  geocoder: false, // 搜索功能
+  homeButton: false, // home
+  sceneModePicker: false, // 隐藏二三维切换
+  baseLayerPicker: false, //隐藏默认底图选择控件
+  navigationHelpButton: false, // 帮助？号
+  animation: false,
+  infoBox: false,
+  timeline: false,
+  fullscreenButton: false,
+  // 使用ArcGIS 底图
+  imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
+    url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
+  }),
+  terrain: Cesium.Terrain.fromWorldTerrain(),
+});
+viewer.cesiumWidget.creditContainer.style.display = "none"; // 去除logo
+viewer.scene.globe.depthTestAgainstTerrain = true;
+
+viewer.camera.flyTo({
+  destination: Cesium.Cartesian3.fromDegrees(
+    103.66553274314929,
+    31.341147649314625,
+    5000
+  ),
+});
+
+// 设置等高线样式
+let globe = viewer.scene.globe;
+let material = new Cesium.Material.fromType("ElevationRamp", {
+  image: "./img/snow.webp",
+  minimumHeight: 1000,
+  maximumHeight: 8000.0,
+});
+globe.material = material;
