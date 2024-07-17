99da6549ddeb792a7b6cdf4ec9122478df13d33d
pensiveant <142905570003@qq.com>
2024-07-17 10:59:51 +0800

    feat: Entity示例，使用initViewer改写

--- src/demo/Entity/js/plane.js
+++ src/demo/Entity/js/plane.js
@@ -1,25 +1,8 @@
 // const Cesium = require("cesium"); // 智能提示使用
 
-Cesium.Ion.defaultAccessToken = defaultAccessToken;
-// 初始化view
-const viewer = new Cesium.Viewer("cesiumContainer", {
-  geocoder: false, // 搜索功能
-  homeButton: false, // home
-  sceneModePicker: false, // 隐藏二三维切换
-  baseLayerPicker: false, //隐藏默认底图选择控件
-  navigationHelpButton: false, // 帮助？号
-  animation: false,
-  infoBox: false,
-  timeline: false,
-  fullscreenButton: false,
-  // 使用ArcGIS 底图
-  imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
-    url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
-  }),
-  terrain: Cesium.Terrain.fromWorldTerrain(),
-});
-viewer.cesiumWidget.creditContainer.style.display = "none"; // 去除logo
-viewer.scene.globe.depthTestAgainstTerrain = true;
+import createViewer from "../../js/initViewer.js";
+const viewer = createViewer();
+
 
 const centerPoint = [104.06335998805471, 30.659858531850176, 500]; // 中心点的经度、纬度、高程
 const point = new Cesium.Entity({
