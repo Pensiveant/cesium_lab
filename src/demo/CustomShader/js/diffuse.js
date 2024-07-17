363b4c57205117601f7578d2e504950f2b81dd38
pensiveant <142905570003@qq.com>
2024-07-17 09:16:36 +0800

    feat： CustomShader内的示例，使用initViewer改写

--- src/demo/CustomShader/js/diffuse.js
+++ src/demo/CustomShader/js/diffuse.js
@@ -1,25 +1,8 @@
 // const Cesium = require("cesium"); // 智能提示使用
 
-// 初始化view
-Cesium.Ion.defaultAccessToken = defaultAccessToken;
-const viewer = new Cesium.Viewer("cesiumContainer", {
-  geocoder: false, // 搜索功能
-  homeButton: false, // home
-  sceneModePicker: false, // 隐藏二三维切换
-  baseLayerPicker: false, //隐藏默认底图选择控件
-  navigationHelpButton: false, // 帮助？号
-  animation: false,
-  infoBox: false,
-  timeline: false,
-  fullscreenButton: false,
-  // 使用ArcGIS 底图
-  imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
-    url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
-  }),
-  terrain: Cesium.Terrain.fromWorldTerrain(),
-});
-viewer.cesiumWidget.creditContainer.style.display = "none"; // 去除logo
-window.viewer = viewer;
+import createViewer from "../../js/initViewer.js"
+const viewer = createViewer();
+
 
 // 加载白模
 let customShader = new Cesium.CustomShader({
